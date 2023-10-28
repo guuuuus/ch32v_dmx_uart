bce82d9f12836e8969443401f3966a77d660ae07
guus <guus@dana>
2023-10-28 09:27:54 +0200

    first commit

---
+++ dmx_ch32v.c
@@ -0,0 +1,379 @@
+#include <dmx_ch32v.h>
+// #include "ltc_ch32v.h"
+
+void dmx_txIRQ(void);
+void dmx_rxIRQ(void);
+void dmx_irqplaceholder();
+
+volatile unsigned char dmx_data[512];
+volatile unsigned short dmx_poscoutner = 0;
+volatile unsigned char dmx_bkcounter = 0;
+unsigned char dmx_bktime = 6; // can be changed in, 4 sems to go well withing spec,
+volatile unsigned short dmx_idlecounter = 0;
+unsigned short dmx_idletime = 2;
+
+void (*dmx_interrupt)(void) = &dmx_irqplaceholder;
+
+volatile unsigned char dmx_startcode = 0xcc;
+// volatile unsigned char dmx_startcode = 0x00;
+volatile unsigned char dmx_newdata = 0x00;
+volatile unsigned char dmx_pktsize;
+volatile dmx_state_t dmx_state = DMX_IDLE;
+volatile dmx_dir_t dmx_dir;
+
+USART_InitTypeDef dmx_uartinit;
+GPIO_InitTypeDef dxm_gpiotx;
+
+void dmx_irqplaceholder()
+{
+    USART_ClearITPendingBit(UARTNUM, 0xffff);
+}
+
+void dmx_beginRX()
+{
+    GPIO_InitTypeDef dxm_gpiorx;
+
+    // dir for it
+    dmx_dir = DMX_RX;
+
+    // peripheral clock
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOPORT, ENABLE);
+#if defined(DMX_UART1)
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_USARTNUM, ENABLE);
+#endif
+
+    dxm_gpiorx.GPIO_Pin = GPIORXPIN;
+    dxm_gpiorx.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+    dxm_gpiorx.GPIO_Speed = GPIO_Speed_2MHz;
+    GPIO_Init(GPIOPORT, &dxm_gpiorx);
+
+    dmx_uartinit.USART_BaudRate = 250000;
+    dmx_uartinit.USART_WordLength = USART_WordLength_8b;
+    dmx_uartinit.USART_StopBits = USART_StopBits_1; // is specified with 2, but the second is just idle time in this case.
+    dmx_uartinit.USART_Parity = USART_Parity_No;
+    dmx_uartinit.USART_Mode = USART_Mode_Rx;
+    dmx_uartinit.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+
+    dmx_interrupt = &dmx_rxIRQ;
+
+    USART_DeInit(UARTNUM);
+    USART_Init(UARTNUM, &dmx_uartinit);
+    USART_ITConfig(UARTNUM, USART_IT_RXNE, ENABLE);
+
+    NVIC_EnableIRQ(USARTNUM_IRQn);
+
+    USART_Cmd(UARTNUM, ENABLE);
+}
+
+void dmx_beginTX()
+{
+    // dir for it
+    dmx_dir = DMX_TX;
+
+    // peripheral clock
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOPORT, ENABLE);
+#if defined(DMX_UART1)
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_USARTNUM, ENABLE);
+#elif defined(DMX_UART2) || (DMX_UART3)
+    RCC_APB1PeriphClockCmd(RCC_APB2Periph_USARTNUM, ENABLE);
+#endif
+
+    // setup the gpio
+
+    dxm_gpiotx.GPIO_Pin = GPIOTXPIN;
+    // dxm_gpiotx.GPIO_Mode = GPIO_Mode_AF_PP;
+    dxm_gpiotx.GPIO_Mode = GPIO_Mode_Out_PP;
+    dxm_gpiotx.GPIO_Speed = GPIO_Speed_2MHz;
+    GPIO_Init(GPIOPORT, &dxm_gpiotx);
+
+    // seting the config
+    dmx_uartinit.USART_BaudRate = 250000;
+    dmx_uartinit.USART_WordLength = USART_WordLength_8b;
+    dmx_uartinit.USART_StopBits = USART_StopBits_2;
+    dmx_uartinit.USART_Parity = USART_Parity_No;
+    dmx_uartinit.USART_Mode = USART_Mode_Tx;
+    dmx_uartinit.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+
+    dmx_interrupt = &dmx_txIRQ;
+
+    USART_DeInit(UARTNUM);
+    USART_Init(UARTNUM, &dmx_uartinit);
+    USART_ITConfig(UARTNUM, USART_IT_TXE, ENABLE);
+
+    NVIC_EnableIRQ(USARTNUM_IRQn);
+
+    USART_Cmd(UARTNUM, ENABLE);
+}
+
+void dmx_stop(){
+    //itcs
+    USART_ITConfig(UARTNUM, USART_IT_TXE, DISABLE);
+    USART_ITConfig(UARTNUM, USART_IT_RXNE, DISABLE);
+    // uart peri
+    USART_Cmd(UARTNUM, DISABLE);
+
+    // peripheral clock
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOPORT, DISABLE);
+#if defined(DMX_UART1)
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_USARTNUM, DISABLE);
+#elif defined(DMX_UART2) || (DMX_UART3)
+    RCC_APB1PeriphClockCmd(RCC_APB2Periph_USARTNUM, DISABLE);
+#endif
+
+}
+
+void dmx_setBreakTime(unsigned short usec)
+{
+
+    if (usec < 132)
+    {
+        dmx_bktime = 3;
+    }
+    else if (usec > 10000)
+    {
+        dmx_bktime = 255;
+    }
+    else
+    {
+        dmx_bktime = usec / 44;
+    }
+}
+
+void dmx_setFPS(dmx_speed_fps f)
+{
+    switch (f)
+    {
+    case DMX_FPS_20:
+    case DMX_FPS_MIN:
+        dmx_bktime = 12;
+        dmx_idletime = 605;
+        break;
+
+    case DMX_FPS_25:
+        dmx_bktime = 12;
+        dmx_idletime = 380;
+        break;
+
+    case DMX_FPS_30:
+        dmx_bktime = 10;
+        dmx_idletime = 230;
+        break;
+
+    case DMX_FPS_35:
+        dmx_bktime = 8;
+        dmx_idletime = 122;
+        break;
+
+    case DMX_FPS_40:
+        dmx_bktime = 6;
+        dmx_idletime = 45;
+        break;
+
+    case DMX_FPS_44:
+    case DMX_FPS_MAX:
+        dmx_bktime = 5;
+        dmx_idletime = 2;
+        break;
+
+    default:
+        dmx_bktime = 12;
+        dmx_idletime = 230;
+        break;
+    }
+}
+
+signed short dmx_setValue(unsigned short chan, unsigned char value)
+{
+    if ((chan > 511) || (dmx_dir == DMX_RX)) // out of range or in rx mode
+        return -1;
+    dmx_data[chan] = value;
+    return 0;
+}
+
+signed short dmx_setValues(unsigned short startchan, unsigned char *p, unsigned short len)
+{
+    if (((startchan + len) > 511) || (dmx_dir == DMX_RX)) // out of range or in rx mode
+        return -1;
+    for (unsigned short i = 0; i < len; i++)
+    {
+        dmx_data[startchan + i] = p[i];
+    }
+    return 0;
+}
+
+void dmx_setStartcode(unsigned char sc)
+{
+    dmx_startcode = sc;
+}
+
+signed short dmx_getValue(unsigned short chan)
+{
+    if (chan > 511) // out of range
+    {
+        return -1;
+    }
+    return (0x00ff & dmx_data[chan]);
+}
+
+signed short dmx_getValues(unsigned short startchan, unsigned char *p, unsigned short len)
+{
+    if ((startchan + len) > 511) // out of range
+        return -1;
+    for (unsigned short i = 0; i < len; i++)
+    {
+        p[i] = dmx_data[startchan + i];
+    }
+    return 0;
+}
+
+unsigned char dmx_getStartcode()
+{
+    return dmx_startcode;
+}
+
+unsigned char dmx_newPacket()
+{
+    unsigned char r = 0;
+    if (dmx_newdata)
+    {
+        r = 0xff;
+        dmx_newdata = 0x00;
+    }
+    return r;
+}
+
+unsigned short dmx_getPacketSize()
+{
+    return dmx_pktsize;
+}
+
+void dmx_txIRQ()
+{
+    USART_ClearITPendingBit(UARTNUM, 0xffff);
+    switch (dmx_state)
+    {
+    case DMX_IDLE:
+        // wait one txe cyle for addr 512 to complete sending
+        if (dmx_idlecounter == 1)
+        {
+            GPIO_WriteBit(GPIOPORT, GPIOTXPIN, 1);
+            dxm_gpiotx.GPIO_Mode = GPIO_Mode_Out_PP;
+            dxm_gpiotx.GPIO_Speed = GPIO_Speed_2MHz;
+            GPIO_Init(GPIOPORT, &dxm_gpiotx);
+        }
+        if ((dmx_idlecounter > dmx_idletime) && (dmx_idletime > 1))
+        {
+            dmx_state = DMX_BREAK;
+        }
+        dmx_idlecounter++;
+        USART_SendData(UARTNUM, 0xff);
+        break;
+
+    case DMX_BREAK:
+        // reset counters
+        dmx_poscoutner = 0;
+        dmx_idlecounter = 0;
+        // set line low
+        GPIO_WriteBit(GPIOPORT, GPIOTXPIN, 0);
+
+        dmx_bkcounter++;
+
+        if (dmx_bkcounter > dmx_bktime)
+        {
+            dmx_state = DMX_START;
+            // next byte is going to be in the reg while switching the gpio from io to uart igues...
+            USART_SendData(UARTNUM, 0xff);
+        }
+        else
+        {
+            USART_SendData(UARTNUM, 0x00);
+        }
+
+        break;
+
+    case DMX_START:
+        dmx_bkcounter = 0;
+
+        dxm_gpiotx.GPIO_Mode = GPIO_Mode_AF_PP;
+        GPIO_Init(GPIOPORT, &dxm_gpiotx);
+        USART_SendData(UARTNUM, dmx_startcode);
+
+        dmx_state = DMX_RUN;
+        break;
+
+    case DMX_RUN:
+        USART_SendData(UARTNUM, dmx_data[dmx_poscoutner]);
+        dmx_poscoutner++;
+        if (dmx_poscoutner >= 512)
+        {
+            dmx_state = DMX_IDLE;
+            dmx_newdata = 0xff;
+            GPIO_WriteBit(GPIOPORT, GPIOTXPIN, 1);
+        }
+        break;
+
+    default:
+        dmx_state = DMX_IDLE;
+        break;
+    }
+}
+
+void dmx_rxIRQ()
+{
+    unsigned char fe = USART_GetFlagStatus(UARTNUM, USART_FLAG_FE);
+    unsigned char data = (0xff & USART_ReceiveData(UARTNUM));
+
+    if (fe)
+        dmx_state = DMX_BREAK;
+
+    switch (dmx_state)
+    {
+    case DMX_BREAK:
+        if (fe)
+        {
+            dmx_poscoutner = 0;
+            dmx_state = DMX_START;
+            dmx_newdata = 0xff;
+        }
+        break;
+
+    case DMX_START:
+        dmx_startcode = data;
+        dmx_state = DMX_RUN;
+        break;
+
+    case DMX_RUN:
+        dmx_data[dmx_poscoutner] = data;
+        dmx_poscoutner++;
+        if (dmx_poscoutner >= 512)
+        {
+            dmx_state = DMX_BREAK;
+        }
+        break;
+    default:
+        break;
+    }
+}
+#if defined(DMX_UART1)
+void USART1_IRQHandler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
+void USART1_IRQHandler()
+{
+    dmx_interrupt();
+}
+#endif
+
+#if defined(DMX_UART2)
+void USART2_IRQHandler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
+void USART2_IRQHandler()
+{
+    dmx_interrupt();
+}
+#endif
+
+#if defined(DMX_UART3)
+void USART3_IRQHandler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
+void USART3_IRQHandler()
+{
+    dmx_interrupt();
+}
+#endif
---
+++ dmx_ch32v.h
@@ -0,0 +1,177 @@
+/*
+Guus 2023. channel vals are 0-511 (NOT 1-512)
+tested on ch32v003 and ch32v203.
+This library does not set the rs485 driver in the right direction. Do so before starting or permanent by hardware.
+*/
+
+#ifndef dmx_ch32v_h
+#define dmx_ch32v_h
+#define DMX_UART1 // defines the uart to use
+
+#if defined(CH32V00X)
+#include <ch32v00x.h>
+#include <ch32v00x_gpio.h>
+#include <ch32v00x_rcc.h>
+#include <ch32v00x_usart.h>
+#define GPIOPORT GPIOD
+#define RCC_APB2Periph_GPIOPORT RCC_APB2Periph_GPIOD
+#define GPIOTXPIN GPIO_Pin_5
+#define GPIORXPIN GPIO_Pin_6
+
+#define UARTNUM USART1
+#define RCC_APB2Periph_USARTNUM RCC_APB2Periph_USART1
+#define USARTNUM_IRQn USART1_IRQn
+
+#if !defined(DMX_UART1)
+#define DMX_UART1
+#endif
+
+#endif
+
+#if defined(CH32V10X)
+#include <ch32v10x.h>
+#include <ch32v10x_gpio.h>
+#include <ch32v10x_rcc.h>
+#include <ch32v10x_usart.h>
+#endif
+
+#if defined(CH32V20X)
+#include <ch32v20x.h>
+#include <ch32v20x_gpio.h>
+#include <ch32v20x_rcc.h>
+#include <ch32v20x_usart.h>
+
+#endif
+
+#if defined(CH32V30X)
+#include <ch32v30x.h>
+#include <ch32v30x_gpio.h>
+#include <ch32v30x_rcc.h>
+#include <ch32v30x_usart.h>
+#endif
+
+#if defined CH32V30X || CH32V20X || CH32V30X
+#if defined(DMX_UART2)
+#define GPIOPORT GPIOA
+#define RCC_APB2Periph_GPIOPORT RCC_APB2Periph_GPIOA
+#define GPIOTXPIN GPIO_Pin_2
+#define GPIORXPIN GPIO_Pin_3
+
+#define UARTNUM USART2
+#define RCC_APB2Periph_USARTNUM RCC_APB1Periph_USART2
+#define USARTNUM_IRQn USART2_IRQn
+
+#elif defined(DMX_UART3)
+#define GPIOPORT GPIOB
+#define RCC_APB2Periph_GPIOPORT RCC_APB2Periph_GPIOB
+#define GPIOTXPIN GPIO_Pin_9
+#define GPIORXPIN GPIO_Pin_10
+
+#define UARTNUM USART3
+#define RCC_APB2Periph_USARTNUM RCC_APB1Periph_USART2
+#define USARTNUM_IRQn USART3_IRQn
+
+#else // use uart1
+#define GPIOPORT GPIOA
+#define RCC_APB2Periph_GPIOPORT RCC_APB2Periph_GPIOA
+#define GPIOTXPIN GPIO_Pin_9
+#define GPIORXPIN GPIO_Pin_10
+
+#define UARTNUM USART1
+#define RCC_APB2Periph_USARTNUM RCC_APB2Periph_USART1
+#define USARTNUM_IRQn USART1_IRQn
+#if !defined(DMX_UART1)
+#define DMX_UART1
+#endif
+#endif
+#endif
+
+extern volatile unsigned char dmx_data[512];
+extern volatile unsigned char dmx_startcode;
+extern volatile unsigned char dmx_newdata;
+extern volatile unsigned char dmx_pktsize;
+
+typedef enum
+{
+    DMX_STOP = 0x00,
+    DMX_IDLE = 0x01,
+    DMX_BREAK = 0x02,
+    DMX_START = 0x03,
+    DMX_RUN = 0x04
+} dmx_state_t;
+
+
+typedef enum
+{
+    DMX_DIRNOTSET = 0x00,
+    DMX_RX = 0x01,
+    DMX_TX = 0x02,
+
+} dmx_dir_t;
+
+// avail dmx speeds in fps. Changing the speed wil change the idle time between frames.
+typedef enum
+{
+
+    DMX_FPS_MIN,
+    DMX_FPS_20,
+    DMX_FPS_25,
+    DMX_FPS_30,
+    DMX_FPS_35,
+    DMX_FPS_40,
+    DMX_FPS_44,
+    DMX_FPS_MAX,
+
+} dmx_speed_fps;
+
+//starts the transmitter
+void dmx_beginTX();
+#define dmx_transmitter_begin dmx_beginTX
+
+// starts the receiver
+void dmx_beginRX();
+#define dmx_receiver_begin dmx_beginRX
+
+// sets (an approximation) of the breaktime in usec. 
+void dmx_setBreakTime(unsigned short usec);
+
+// sets the speed of tx mode
+void dmx_setFPS(dmx_speed_fps f);
+
+// sets single value to addr
+signed short dmx_setValue(unsigned short chan, unsigned char value);
+
+// sets vals from arr to dmx
+signed short dmx_setValues(unsigned short startchan, unsigned char *p, unsigned short len);
+
+// set the startcode
+void dmx_setStartcode(unsigned char sc);
+
+// get single value from a channel
+signed short dmx_getValue(unsigned short chan);
+
+// get values from dmx into arr
+signed short dmx_getValues(unsigned short startchan, unsigned char *p, unsigned short len);
+
+// returns last packet startcode
+unsigned char dmx_getStartcode();
+
+// returns 0xff if new frame is avail since last check 
+unsigned char dmx_newPacket();
+
+// returns size of last received packet
+unsigned short dmx_getPacketSize();
+
+
+#if defined(DMX_UART1)
+extern void USART1_IRQHandler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
+#endif
+
+#if defined(DMX_UART2)
+extern void USART2_IRQHandler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
+#endif
+
+#if defined(DMX_UART3)
+extern void USART3_IRQHandler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
+#endif
+#endif
